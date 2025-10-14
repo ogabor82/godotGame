30feb5fd1c6e9f5f509b6e6f958cb0eaa342ca6f
Ocskó Gábor <ogabor@gmail.com>
2025-10-14 14:13:23 +0200

    Add Warrior_Run texture and update player scene to include running animation; modify player script for animation control

--- scripts/player.gd
+++ scripts/player.gd
@@ -21,7 +21,10 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.x == 0:
+			$AnimatedSprite2D.play("idle")
 
 	move_and_slide()
