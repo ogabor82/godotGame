5fad1a24fefe0986d50e1dabb24408c7c7de192d
Ocskó Gábor <ogabor@gmail.com>
2025-10-14 11:22:14 +0200

    Add support for jump action using the up arrow key in player script

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,6 +12,9 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	# handle jump with up arrow
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
